37ef95526d9b0329a4f6324b17da34e7130d623d
TolDann <danny.tolkendorf88@gmail.com>
2022-10-23 12:34:35 +0200

    Einfache Displayeingabe und einfache Rechnung mit der Displayeingabe. - Eingabe wird in 2 Zahlen eingeteilt. - Der Rechenoperator wird ermittelt und verwendet. - Das Ergebnis wird angezeigt. - Falsche Eingabe wird erkannt (Wenn keine zwei Zahlen eingeben wurden oder Rechenoperator falsch ist bzw. fehlt).
    Hinweis: Vorzeichen werden nicht erkannt bzw. ist fehlerhaft, Rechnungen mit positiven zahlen funktionieren schon.
    - Zum Rechnen wird eine Rechnung eingeben z.B. 10+5 und anschließend auf den Button <=> geklickt.
       Das Ergebnis erscheint im Display.

---
+++ Taschenrechner/Classes/Calculate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Taschenrechner.Classes
+{
+    internal class Calculate
+    {
+        public double _Number1;
+        public double _Number2;
+        public char _Operator;
+        public double _Result;
+
+        public void GetResult()
+        {
+            switch (_Operator)
+            {
+                case '+':
+                    _Result = _Number1 + _Number2;
+                    break;
+                case '-':
+                    _Result = _Number1 - _Number2;
+                    break;
+                case '/':
+                    _Result = _Number1 / _Number2;
+                    break;
+                case '*':
+                    _Result = _Number1 * _Number2;
+                    break;
+            }
+        }
+    }
+}
--- Taschenrechner/MainWindow.xaml.cs
+++ Taschenrechner/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Taschenrechner.Classes;
 
 namespace Taschenrechner
 {
@@ -24,5 +25,64 @@ namespace Taschenrechner
         {
             InitializeComponent();
         }
+
+        private void btn_Result_Click(object sender, RoutedEventArgs e)
+        {
+            ReadDisplayInput();
+        }
+
+        void ReadDisplayInput()
+        {
+            string input = txb_Display.Text;
+            Calculate cal = new Calculate();
+
+            if (UserInputIsRight(input, cal) && CanCalculate(input, cal))
+            {
+                cal.GetResult();
+            }
+            else
+            {
+                MessageBox.Show("Falsche Eingabe!");
+            }
+
+            txb_Display.Text = cal._Result.ToString();
+        }
+
+        bool UserInputIsRight(string input, Calculate cal)
+        {
+            bool isRight = false;
+            for (int i = 0; i < Operator().Length; i++)
+            {
+                if (input.Contains(Operator()[i]))
+                {
+                    isRight = true;
+                    cal._Operator = Operator()[i];
+                    break;
+                }
+            }
+            return isRight;
+        }
+
+        bool CanCalculate(string input, Calculate cal)
+        {
+            bool areNumbers = false;
+            string[] nums = input.Split(cal._Operator);
+            try
+            {
+                cal._Number1 = Convert.ToDouble(nums[0]);
+                cal._Number2 = Convert.ToDouble(nums[1]);
+                areNumbers = true;
+            }
+            catch (Exception) { }
+
+            return areNumbers;
+        }
+
+        char[] Operator()
+        {
+            return new char[] { '+', '-', '*', '/' };
+        }
+
+
     }
 }
