a06e028a62fcd1638a5bca4ec5230c4266433a84
TolDann <danny.tolkendorf88@gmail.com>
2022-10-29 10:10:35 +0200

    Methoden gelöscht. - Alle Methoden die für das auswerrten des Displaytextes waren würden gelöscht.
    Display TextBox wurde zu einem TextBlock.
    - Die Recheneingabe soll erstmal nur mit Mausklicks eingegeben werden können.
    
    Clickevent für alle Zahlen-Buttons hinzugefügt.
    - Eine Methode für alle Zahlen-Buttons.
    - Der Buttoncontent wird als String an eine Methode übergeben.
    
    BuildANumber Methode hhinzugefügt.
    - liest den Displaytext und fügt an diesem die neue Zahl hinzu.
    - neuer Displaytext wird in das Display geschrieben.

--- Taschenrechner/MainWindow.xaml.cs
+++ Taschenrechner/MainWindow.xaml.cs
@@ -28,55 +28,7 @@ namespace Taschenrechner
 
         private void btn_Result_Click(object sender, RoutedEventArgs e)
         {
-            ReadDisplayInput();
-        }
-
-        void ReadDisplayInput()
-        {
-            string input = txb_Display.Text;
-            Calculate cal = new Calculate();
-
-            if (UserInputIsRight(input, cal) && CanCalculate(input, cal))
-            {
-                cal.GetResult();
-            }
-            else
-            {
-                MessageBox.Show("Falsche Eingabe!");
-            }
-            txb_Display.Text = $"{cal._Result}";
-        }
-
-        bool UserInputIsRight(string input, Calculate cal)
-        {
-            bool isRight = false;
-
-            for (int i = 0; i < Operator().Length; i++)
-            {
-                if (input.Contains(Operator()[i]))
-                {
-                    isRight = true;
-                    cal._Operator = Operator()[i];
-                    break;
-                }
-            }
-            return isRight;
-        }
-
-        bool CanCalculate(string input, Calculate cal)
-        {
-            bool areNumbers = false;
-
-            string[] nums = input.Split(cal._Operator);
-            try
-            {
-                cal._Number1 = Convert.ToDouble(nums[0]);
-                cal._Number2 = Convert.ToDouble(nums[1]);
-                areNumbers = true;
-            }
-            catch (Exception){ }
             
-            return areNumbers;
         }
 
         char[] Operator()
@@ -84,6 +36,18 @@ namespace Taschenrechner
             return new char[] { '+', '-', '*', '/' };
         }
 
+        void ButtonClickNumbers(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            string text = button.Content.ToString();
+            BuildANumber(text);
+        }
 
+        void BuildANumber(string input)
+        {
+            string numberInString = txb_Display.Text;
+            numberInString += input;
+            txb_Display.Text = numberInString;
+        }
     }
 }
